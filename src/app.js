b275f5605f7f521916b3b7d985d9ef18c1e8f1d1
timokonkwo <chibuiketimokonkwo@gmail.com>
2022-07-12 22:34:55 +0100

    ui

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ document.addEventListener('DOMContentLoaded', getPosts);
 /* Add posts on clicking post button */
 document.querySelector(".post-submit").addEventListener('click', postSubmit);
 
+/* delete posts on clicking delete button */
+document.querySelector('#posts').addEventListener('click', deletePost);
+
 /* Function to get posts from server */
 function getPosts() {
     http.get('http://localhost:3000/posts')
@@ -19,13 +22,40 @@ function postSubmit() {
     const title = document.querySelector("#title").value
     const body = document.querySelector("#body").value
 
-    const data = { title, body }
+    if (title !== '' && body !== '') {
+        const data = { title, body }
 
-    http.post('http://localhost:3000/posts', data)
-        .then(() => {
-            ui.clearInput();
-            ui.showAlert('Post Added', "alert alert-success");
-            getPosts();
-        })
-        .catch(err => ui.showAlert("An Error Occured", "alert alert-danger"))
+        http.post('http://localhost:3000/posts', data)
+            .then(() => {
+                ui.clearInput();
+                ui.clearAlert();
+                ui.showAlert('Post Added', "alert alert-success");
+                getPosts();
+            })
+            .catch(() => ui.showAlert("An Error Occured", "alert alert-danger"));
+    } else {
+        ui.clearAlert();
+        ui.showAlert('Fill the empty fields', "alert alert-danger");
+    }
+
+
+}
+
+/* Function to delete a post */
+function deletePost(e) {
+    const target = e.target.parentElement;
+
+    if (target.classList.contains('delete')) {
+        const id = target.id;
+
+        if (confirm("Are you sure?")) {
+            http.delete(`http://localhost:3000/posts/${id}`)
+                .then(() => {
+                    ui.clearAlert();
+                    ui.showAlert('Post deleted', 'alert alert-danger');
+                    getPosts();
+                })
+                .catch(() => ui.showAlert("An Error Occured", "alert alert-danger"))
+        }
+    }
 }
--- src/ui.js
+++ src/ui.js
@@ -18,17 +18,17 @@ class UI {
                         <h4 class="card-title">${post.title}</h4>
                         <p class="card-text">${post.body}</p>
 
-                        <a href="#" class="edit card-link" data-id='${post.id}'>
+                        <a href="#" class="edit card-link" data-id="${post.id}">
                             <i class="fa fa-pencil"></i>
                         </a>
 
-                        <a href="#" class="delete card-link">
+                        <a href="#" class="delete card-link" id="${post.id}">
                             <i class="fa fa-remove"></i>
                         </a>
                     </div>
                 </div>
             `
-        })
+        });
 
         this.posts.innerHTML = output;
     }
