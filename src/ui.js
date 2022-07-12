c910ffe5849595f2c1bfc8e73a0af5492db93771
timokonkwo <chibuiketimokonkwo@gmail.com>
2022-07-12 14:06:36 +0100

    ui

--- src/ui.js
+++ src/ui.js
@@ -1,6 +1,6 @@
 class UI {
     constructor() {
-        this.post = document.querySelector('#posts');
+        this.posts = document.querySelector('#posts');
         this.titleInput = document.querySelector('title');
         this.bodyInput = document.querySelector('#body');
         this.idInput = document.querySelector('#id');
@@ -9,7 +9,24 @@ class UI {
     }
 
     showPosts(posts) {
-        console.log(posts);
+        let output = "";
+
+        posts.forEach(post => {
+            output += `
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <h4 class="card-title">${post.title}</h4>
+                        <p class="card-text">${post.body}</p>
+
+                        <a href="#" class="edit">
+                            <i class="fa fa-pencil"></i>
+                        </a>
+                    </div>
+                </div>
+            `
+        })
+
+        this.posts.innerHTML = output;
     }
 }
 
